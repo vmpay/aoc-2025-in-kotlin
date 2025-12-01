f826a35ca48f77d6cba672ed677c3c8e430f9494
Andrew <vmpay.eu@gmail.com>
2025-12-01 18:20:20 +0100

    Day01.kt impl

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,64 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        var position = 50
+        for (line in input) {
+            val direction = line[0]
+            val distance = line.substring(1).toInt()
+            position += if(direction == 'L') -distance else distance
+            position %= 100
+            if (position < 0) {
+                position += 100
+            }
+            if (position == 0) {
+                result++
+            }
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        var position = 50
+        for (line in input) {
+            val direction = line[0]
+            val distance = line.substring(1).toInt()
+            var adjuster = 0
+            when (direction) {
+                'L' -> {
+                    if (position == 0) {
+                        adjuster--
+                    }
+                    if(position <= distance){
+                        adjuster++
+                    }
+                    position -= distance
+                }
+
+                else -> {
+                    position += distance
+                }
+            }
+            adjuster += abs(position / 100)
+            result += adjuster
+            position %= 100
+            if (position < 0) {
+                position += 100
+            }
+        }
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
+    // Read a large test input from the `src/DayXX_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
+    // Read the input from the `src/DayXX.txt` file.
     val input = readInput("Day01")
+    println("Final results")
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day07.kt
@@ -0,0 +1,22 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // Read a large test input from the `src/DayXX_test.txt` file:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    // Read the input from the `src/DayXX.txt` file.
+    val input = readInput("Day07")
+    println("Final results")
+    part1(input).println()
+    part2(input).println()
+}
